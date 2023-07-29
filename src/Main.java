1eebe92c99bf19438a59e4f786c7b12bfa060897
Alex Belkin <dustiklove@yandex.ru>
2023-07-29 21:31:29 +0300

    files ignored

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 11572; // Сумма покупки
+        int amountmile = 20; // Сумма для одной мили
+
+        int bonus = (amount / amountmile);
+
+        System.out.println("Итоговый бонус: " + bonus);
+
+    }
+}
+
